0ba2ced22576c2ab845e6e792c57269214cb4315
NisalDeZoysa <90953837+NisalDeZoysa@users.noreply.github.com>
2024-01-18 17:28:07 +0530

    first commit

---
+++ lib/components/my_textfiels.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyTextField({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText, // Fix the property name here
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText, // Use the correct property name here
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.green),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.red),
+          ),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+}
